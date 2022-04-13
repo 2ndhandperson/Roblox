f03d9734819d78b2cb884916aca866905517c891
2ndhandperson <103535981+2ndhandperson@users.noreply.github.com>
2022-04-12 20:40:36 -0400

    Update Align.lua

--- Align.lua
+++ Align.lua
@@ -1,7 +1,7 @@
 local plr = game.Players.LocalPlayer
 local chr = plr.Character
-getgenv() function Align(hat, part, a, b, c, d, e, f)
-    local hat2 = chr["hat"] -- The hat you choose
+getgenv() function Alignhat(hat, part, a, b, c, d, e, f)
+    local hat2 = chr[hat].Handle -- The hat you choose
     hat2:BreakJoints()
     local Weld = Instance.new("Weld", game.Players.LocalPlayer.Character)
 
@@ -9,7 +9,7 @@ getgenv() function Align(hat, part, a, b, c, d, e, f)
     Weld.Part0 = part -- What your welding the hat to (has to be a BasePart)
 
     Weld.C0 = CFrame.new(a,b,c)*CFrame.Angles(math.rad(d),math.rad(e),math.rad(f)) -- Offset & Angles
-    print("Ran with no errors")
+    print("Aligned Hats")
 end
 getgenv() function Destroy_Mesh(hat)
     local hat1 = game:GetService("Workspace").non[game.Players.LocalPlayer.Name][hat].Handle:FindFirstChildOfClass("SpecialMesh") or game.Players.LocalPlayer.Character[hat].Handle:FindFirstChildOfClass("SpecialMesh")
@@ -29,4 +29,3 @@ getgenv() function Make_Parts_Transparent(part)
         end
     end
 end
-
