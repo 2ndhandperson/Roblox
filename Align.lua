a874e98c45395c3fe438345ac42e753d0b059629
2ndhandperson <103535981+2ndhandperson@users.noreply.github.com>
2022-04-14 18:36:14 -0400

    Update Align.lua

--- Align.lua
+++ Align.lua
@@ -11,6 +11,17 @@ getgenv() function Alignhat(hat, part, a, b, c, d, e, f)
     Weld.C0 = CFrame.new(a,b,c)*CFrame.Angles(math.rad(d),math.rad(e),math.rad(f)) -- Offset & Angles
     print("Ran with no errors")
 end
+getgenv() function Alignfling(fling, part1, g, h, i, j, k, l)
+    local fling2 = chr[fling] -- The hat you choose
+    fling2:BreakJoints()
+    local Weld = Instance.new("Weld", game.Players.LocalPlayer.Character)
+
+    Weld.Part1 = fling2 -- (Hat)
+    Weld.Part0 = part1 -- What your welding the hat to (has to be a BasePart)
+
+    Weld.C0 = CFrame.new(a,b,c)*CFrame.Angles(math.rad(d),math.rad(e),math.rad(f)) -- Offset & Angles
+    print("Ran with no errors")
+end
 getgenv() function clearmesh(hat)
     local hat1 = game.Players.LocalPlayer.Character.Reanimate[hat].Handle:FindFirstChildOfClass("SpecialMesh") or game.Players.LocalPlayer.Character[hat].Handle:FindFirstChildOfClass("SpecialMesh")
     hat1:Destroy()
