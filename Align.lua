690a06ab9e37666dc6740db6a058685b5fd9e829
2ndhandperson <103535981+2ndhandperson@users.noreply.github.com>
2022-04-14 16:21:39 -0400

    Update Align.lua

--- Align.lua
+++ Align.lua
@@ -1,7 +1,7 @@
 local plr = game.Players.LocalPlayer
 local chr = plr.Character
 getgenv() function Alignhat(hat, part, a, b, c, d, e, f)
-    local hat2 = chr[hat] -- The hat you choose
+    local hat2 = chr[hat].Handle -- The hat you choose
     hat2:BreakJoints()
     local Weld = Instance.new("Weld", game.Players.LocalPlayer.Character)
 
@@ -9,36 +9,13 @@ getgenv() function Alignhat(hat, part, a, b, c, d, e, f)
     Weld.Part0 = part -- What your welding the hat to (has to be a BasePart)
 
     Weld.C0 = CFrame.new(a,b,c)*CFrame.Angles(math.rad(d),math.rad(e),math.rad(f)) -- Offset & Angles
-    print("Aligned Hats")
+    print("Ran with no errors")
 end
-getgenv() function Alignfling(hat3, part2, g, h, i, j, k, l)
-    local hat4 = chr.Reanimate[hat3] -- The hat you choose
-    hat4:BreakJoints()
-    local Weld = Instance.new("Weld", game.Players.LocalPlayer.Character)
-
-    Weld.Part1 = hat4 -- (Hat)
-    Weld.Part0 = part2 -- What your welding the hat to (has to be a BasePart)
-
-    Weld.C0 = CFrame.new(g,h,i)*CFrame.Angles(math.rad(j),math.rad(k),math.rad(l)) -- Offset & Angles
-    print("Aligned fling")
-end
-getgenv() function makepart(hatname, newname)
-    chr[hatname].Handle.Name = newname
-    chr[hatname][newname].Parent = chr
-    chr[hatname]:destroy()
-end
-getgenv() function Destroy_Mesh(hat)
-    local hat1 = game.Players.LocalPlayer.Character.Reanimate[hat]:FindFirstChildOfClass("SpecialMesh") or game.Players.LocalPlayer.Character[hat]:FindFirstChildOfClass("SpecialMesh")
+getgenv() function clearmesh(hat)
+    local hat1 = game.Players.LocalPlayer.Character.Reanimate[hat].Handle:FindFirstChildOfClass("SpecialMesh") or game.Players.LocalPlayer.Character[hat].Handle:FindFirstChildOfClass("SpecialMesh")
     hat1:Destroy()
 end
-getgenv() function Lerp_CFrame(part, cframe, duration)
-	for i=0, 1, 0.01*duration do
-		wait()
-		part.CFrame = part.CFrame:Lerp(cframe, i)
-
-	end
-end
-getgenv() function Make_Parts_Transparent(part)
+getgenv() function makepartsinvis(part)
     for _,v in pairs(part:GetDescendants()) do
         if v:IsA("BasePart") then
             v.Transparency = 1
