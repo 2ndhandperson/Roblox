3b9f30a2ba8fc199dafb37ba96aac939299a6706
2ndhandperson <103535981+2ndhandperson@users.noreply.github.com>
2022-04-13 20:41:42 -0400

    Update Align.lua

--- Align.lua
+++ Align.lua
@@ -11,10 +11,10 @@ getgenv() function Alignhat(hat, part, a, b, c, d, e, f)
     Weld.C0 = CFrame.new(a,b,c)*CFrame.Angles(math.rad(d),math.rad(e),math.rad(f)) -- Offset & Angles
     print("Aligned Hats")
 end
-getgenv() function makepart(hat, newname)
-    game.Players.LocalPlayer.Character[hat].Handle.Name = newname
-    game.Players.LocalPlayer.Character[hat][newname].Parent = game.Players.LocalPlayer.Character
-    game.Players.LocalPlayer.Character[hat]:destroy()
+getgenv() function makepart(hatname, newname)
+    game.Players.LocalPlayer.Character[hatname].Handle.Name = newname
+    game.Players.LocalPlayer.Character[hatname][newname].Parent = game.Players.LocalPlayer.Character
+    game.Players.LocalPlayer.Character[hatname]:destroy()
 end
 getgenv() function Destroy_Mesh(hat)
     local hat1 = game.Players.LocalPlayer.Character.Reanimate[hat].Handle:FindFirstChildOfClass("SpecialMesh") or game.Players.LocalPlayer.Character[hat].Handle:FindFirstChildOfClass("SpecialMesh")
