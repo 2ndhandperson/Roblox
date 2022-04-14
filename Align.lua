3b4b07e501137c98e4a2d2f354b2d4391c632ef6
2ndhandperson <103535981+2ndhandperson@users.noreply.github.com>
2022-04-13 20:59:29 -0400

    Update Align.lua

--- Align.lua
+++ Align.lua
@@ -1,6 +1,17 @@
 local plr = game.Players.LocalPlayer
 local chr = plr.Character
-getgenv() function Alignhat(hat, part, a, b, c, d, e, f)
+getgenv() function Align(hat, part, a, b, c, d, e, f)
+    local hat2 = chr[hat] -- The hat you choose
+    hat2:BreakJoints()
+    local Weld = Instance.new("Weld", game.Players.LocalPlayer.Character)
+
+    Weld.Part1 = hat2 -- (Hat)
+    Weld.Part0 = part -- What your welding the hat to (has to be a BasePart)
+
+    Weld.C0 = CFrame.new(a,b,c)*CFrame.Angles(math.rad(d),math.rad(e),math.rad(f)) -- Offset & Angles
+    print("Aligned Hats")
+end
+getgenv() function Alignreanim(hat, part, a, b, c, d, e, f)
     local hat2 = chr.Reanimate[hat] -- The hat you choose
     hat2:BreakJoints()
     local Weld = Instance.new("Weld", game.Players.LocalPlayer.Character)
