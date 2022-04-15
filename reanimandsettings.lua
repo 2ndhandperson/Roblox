e3148d375c8ed3bb645a1bff2d6c452cb8f980f6
2ndhandperson <103535981+2ndhandperson@users.noreply.github.com>
2022-04-14 21:53:42 -0400

    Delete reanimandsettings.lua

--- reanimandsettings.lua
+++
@@ -1,6 +0,0 @@
-_G.ReanimationType = "PDeath"
-_G.FlingBlock = true
-_G.HighlightFlingBlock = true
-_G.FlingBlockPosition = "flingpos"
-_G.HighlightFlingBlockColor = Color3.fromRGB(255,0,215)
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/2ndhandperson/Roblox/main/Reanimation.lua"),true))()
