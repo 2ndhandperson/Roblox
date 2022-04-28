cedd89054b04f38a4950d4a36854791d27947f93
2ndhandperson <103535981+2ndhandperson@users.noreply.github.com>
2022-04-27 20:34:24 -0400

    Update Align.lua

--- Align.lua
+++ Align.lua
@@ -1,34 +1,33 @@
 local plr = game.Players.LocalPlayer
 local chr = plr.Character
-getgenv() function Alignhat(hat, part, a, b, c, d, e, f)
+getgenv() function Align_Hat(hat, part, a, b, c, d, e, f)
     local hat2 = chr[hat].Handle -- The hat you choose
     hat2:BreakJoints()
     local Weld = Instance.new("Weld", game.Players.LocalPlayer.Character)
+    -- Tips for past John (Tescalus)
 
     Weld.Part1 = hat2 -- (Hat)
     Weld.Part0 = part -- What your welding the hat to (has to be a BasePart)
 
     Weld.C0 = CFrame.new(a,b,c)*CFrame.Angles(math.rad(d),math.rad(e),math.rad(f)) -- Offset & Angles
-    print("aligned hat")
+    print("Ran with no errors")
 end
-getgenv() function Alignfling(fling, part1, g, h, i, j, k, l)
-    local fling2 = chr.ExProReanimate.[game.Players.LocalPlayer.Name][fling] -- The hat you choose
-    fling2:BreakJoints()
-    local Weld = Instance.new("Weld", game.Players.LocalPlayer.Character)
-
-    Weld.Part1 = fling2 -- (Hat)
-    Weld.Part0 = part1 -- What your welding the hat to (has to be a BasePart)
-
-    print("aligned flingpos")
-end
-getgenv() function clearmesh(hat)
-    local hat1 = game.Players.LocalPlayer.Character.Reanimate[hat].Handle:FindFirstChildOfClass("SpecialMesh") or game.Players.LocalPlayer.Character[hat].Handle:FindFirstChildOfClass("SpecialMesh")
+getgenv() function Destroy_Mesh(hat)
+    local hat1 = game:GetService("Workspace").non[game.Players.LocalPlayer.Name][hat].Handle:FindFirstChildOfClass("SpecialMesh") or game.Players.LocalPlayer.Character[hat].Handle:FindFirstChildOfClass("SpecialMesh")
     hat1:Destroy()
 end
-getgenv() function makepartsinvis(part)
+getgenv() function Lerp_CFrame(part, cframe, duration)
+	for i=0, 1, 0.01*duration do
+		wait()
+		part.CFrame = part.CFrame:Lerp(cframe, i)
+
+	end
+end
+getgenv() function Make_Parts_Transparent(part)
     for _,v in pairs(part:GetDescendants()) do
         if v:IsA("BasePart") then
             v.Transparency = 1
         end
     end
 end
+warn("This script uses the Pendulum Hub Align Method and Tools. Join the server for high quality scripts https://discord.gg/rgcA9v9PMz")
